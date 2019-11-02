c0824cdb674078e4cee73d3ea4f3bfd121f6b949
Min Soo Kim <42727426+mk374@users.noreply.github.com>
2019-11-02 18:11:54 -0400

    The master file to call all

---
+++ python_scripts/master.py
@@ -0,0 +1,12 @@
+#calls all of the INSERT fiels through one file
+
+import zip as z1
+
+
+
+def calling_all():
+    #adding zipcodes
+    #the below would probably never be called until one is calling it for the first time
+    zip.main()
+
+    #adding users
