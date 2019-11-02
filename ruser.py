a58e5e77e916f77e04dcdf8bb034e5c5324dae43
Benjamin Burnette <bmb51@duke.edu>
2019-11-02 17:51:19 -0400

    what i have so far of ruser

---
+++ ruser.py
@@ -0,0 +1,38 @@
+import psycopg2
+import random
+try:
+   connection = psycopg2.connect(dbname = "justforplay")
+   cursor = connection.cursor()
+    names = ['Dave', 'Sammy', 'Minsoo', 'Benjamin', 'Andy', 'Abby', 'Tommy', 'Ross', 'Rachel','Joey', 'Chandler', 'Phoebe',
+             'Monica', 'Bart', 'Homer', 'Marge', 'Lisa', 'Maggie']
+   snacks = ['crepes', 'goldfish', 'Doritos', 'waffles', 'pancakes', 'pumpkin pie']
+   users = []
+   zips = [27503, 27517, 27560, 27701, 27703, 27704, 27705, 27707, 27709, 27712, 27713]
+   for i in range(1000):
+       rname = random.choice(names)
+       ruid = name + str(i)
+       rpassword = 'password' + str(i)
+       rbio = 'My name is ' + name + ' and I like ' + random.choice[snacks] + '.'
+       rzipcode = random.choice(zips)
+       temp = (rname, ruid, rpassword, rbio, rzipcode)
+       users.append(temp)
+
+   for item in users:
+        postgres_insert_query = """ INSERT INTO ruser (uid, name, password, bio, zip_code) VALUES (%s,%s,%s, %s, %d)"""
+        record_to_insert = item
+        cursor.execute(postgres_insert_query, record_to_insert)
+
+        connection.commit()
+        count = cursor.rowcount
+        print (count, "Record inserted successfully into mobile table")
+
+except (Exception, psycopg2.Error) as error :
+    if(connection):
+        print("Failed to insert record into mobile table", error)
+
+finally:
+    #closing database connection.
+    if(connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
