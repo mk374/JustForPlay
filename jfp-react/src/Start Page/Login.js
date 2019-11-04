e085071b050a2d14b8ab9b963bff7d318338eed7
Andy Ju <andy.ju@duke.edu>
2019-11-03 22:01:03 -0500

    login and register

---
+++ jfp-react/src/Start Page/Login.js
@@ -0,0 +1,79 @@
+import React, {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+import axios from 'axios';
+import HomePage from '../Home/HomePage.js'
+
+// basic styling
+const style = {margin: 15};
+
+// login in component
+class Login extends React.Component {
+    // initialize login page with proper values
+    constructor(props){
+        super(props);
+        this.state={
+            username:'',
+            password:''
+        }
+    }
+
+     handleClick = e =>{
+        // to change in the future for custom routing
+        let apiBaseUrl = "http://localhost:4000/api/";
+        let self = this;
+        let userinfo={
+            "email":this.state.username,
+            "password":this.state.password
+        }
+        // post it to the backend
+        axios.post(apiBaseUrl+'login', userinfo).then(function (response) {
+            console.log(response);
+            if(response.data.code === 200){
+                var uploadScreen=[];
+                uploadScreen.push(<HomePage appContext={self.props.appContext}/>)
+                self.props.appContext.setState({loginPage:[], uploadScreen:uploadScreen})
+            }
+            else if(response.data.code === 204){
+                console.log("Username password do not match");
+                alert("Username password do not match");
+            }
+            else{
+                console.log("Username does not exists");
+                alert("Username does not exist");
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+    // rendering of the login page
+    render() {
+        return (
+          <div>
+            <MuiThemeProvider>
+              <div>
+              <AppBar title="Login"/>
+                <TextField
+                 hintText="Enter your Username"
+                 floatingLabelText="Username"
+                 onChange = {(event, newValue) => this.setState({username:newValue})}/>
+                <br/>
+                <TextField
+                   type="password"
+                   hintText="Enter your Password"
+                   floatingLabelText="Password"
+                   onChange = {(event, newValue) => this.setState({password:newValue})}/>
+                <br/>
+                <RaisedButton label="Submit" primary={true} style={style} onClick={this.handleClick}/>
+             </div>
+             </MuiThemeProvider>
+          </div>
+        );
+      }
+}
+// export
+export default Login;
--- jfp-react/src/index.js
+++ jfp-react/src/index.js
@@ -4,15 +4,12 @@ import './index.css';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
-import Main from './Main';
+import App from './App';
 
 ReactDOM.render((
-    <Router history={history}>
-        <Main history={history} />
-    </Router>
+    <App></App>
 ), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
