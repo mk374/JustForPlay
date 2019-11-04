f9d010ca44230b95dbbcdb814396ef5b65c6e547
Min Soo Kim <42727426+mk374@users.noreply.github.com>
2019-11-04 11:18:10 -0500

    Update models.py

--- models.py
+++ models.py
@@ -16,8 +16,8 @@ class User(db.Model):
 	bio = db.Column('bio', db.String(1024), primary_key=False, nullable=False)
 	zip_code = db.Column('zip_code', db.SmallInteger, db.ForeignKey('zip.zip_code'), nullable=False,) 
 
-# 	members = orm.relationship('Members')
-# 	groups = orm.relationship('Groups')
+	members = orm.relationship('Members')
+	groups = orm.relationship('Groups')
 	
 	
 	
