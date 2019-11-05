2fe7535b1801f4ad9763c2e0dd22e3f9927479d8
Min Soo Kim <42727426+mk374@users.noreply.github.com>
2019-11-04 20:55:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ db = SQLAlchemy(app, session_options={'autocommit': False})
 @app.route('/login', methods=['POST'])
 def check_login():
 	
-	user = db.session.query(models.User).filter(models.User.uid == str(request.data.get('uid')) and \
-						    models.User.password == str(request.data.get('password'))).one()
+	user = db.session.query(models.User).filter(and_(models.User.uid == str(request.data.get('uid')), \
+						    models.User.password == str(request.data.get('password')))).one()
 	if not user:
 		return "Wrong Password and Username"
 	dict_groups = models.User.get_groups(user.uid)
