8f1f35510da0b6225eef39f1f7a38fc7425e02df
abby-franklin811 <57183718+abby-franklin811@users.noreply.github.com>
2019-11-05 17:54:26 -0600

    Add files via upload

---
+++ python_scripts/test-statements.sql
@@ -0,0 +1,24 @@
+
+--example, selecting all the groups that Ballistic Ben is a member of; 1 represents an email that will change
+--depending on the user logged in
+select Groups.gid, group_name, description from groups, members where '1' = Members.uid and Members.gid = Groups.gid;
+
+--example, selecting all the messages from the group ordered by time
+select txt from messages where 'd' = gid order by m_date, m_time asc;
+
+--example selecting all events for a given group
+select * from events where 'a' = gid;
+
+--selecting all members in a given group
+select * from members where 'a' = gid;
+
+--example query for displaying groups within a given distance
+select gid, group_name, Zip.zip_code, description
+from groups, (select latitude, longitude from Zip where 27708 = zip_code) as C, Zip
+where communityid = 'Nightlife' and
+ (2 * 3961 * asin(sqrt((sin(radians((C.latitude - Zip.latitude) / 2))) ^ 2
++ cos(radians(Zip.latitude)) * cos(radians(C.latitude)) * (sin(radians((C.longitude - Zip.longitude) / 2))) ^ 2))) < 0.01
+and groups.public_or_private = 'public';
+
+--example for selecting communities (when the user searches for music)
+select * from community where communityid = 'Music';
