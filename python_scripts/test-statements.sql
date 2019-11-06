81c02bcebc2500d3cd0d71f6546c4f5d1e71ebd0
abby-franklin811 <57183718+abby-franklin811@users.noreply.github.com>
2019-11-05 18:11:37 -0600

    Update test-statements.sql

--- python_scripts/test-statements.sql
+++ python_scripts/test-statements.sql
@@ -1,16 +1,16 @@
 
 --example, selecting all the groups that Ballistic Ben is a member of; 1 represents an email that will change
 --depending on the user logged in
-select Groups.gid, group_name, description from groups, members where '1' = Members.uid and Members.gid = Groups.gid;
+select Groups.gid, group_name, description from groups, members where 'Marge7' = Members.uid and Members.gid = Groups.gid;
 
 --example, selecting all the messages from the group ordered by time
-select txt from messages where 'd' = gid order by m_date, m_time asc;
+select txt from messages where '16Animals16' = gid order by m_date, m_time asc;
 
 --example selecting all events for a given group
-select * from events where 'a' = gid;
+select * from events where '13Philanthropy13' = gid;
 
 --selecting all members in a given group
-select * from members where 'a' = gid;
+select * from members where '69Crafts69' = gid;
 
 --example query for displaying groups within a given distance
 select gid, group_name, Zip.zip_code, description
