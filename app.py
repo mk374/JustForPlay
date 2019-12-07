a69ee2df0cb19846e7486e127780f2ac97aabde0
Min Soo Kim <42727426+mk374@users.noreply.github.com>
2019-12-06 22:21:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, render_template, redirect, url_for
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
 from flask_api import FlaskAPI
 import models
